16bca70f588864dc9474caa552cc2666d8228068
YeachaN <92733150+chans27@users.noreply.github.com>
2024-02-03 18:00:00 +0900

    first commit

---
+++ validation/src/test/java/hello/itemservice/validation/MessageCondesResolverTest.java
@@ -0,0 +1,37 @@
+package hello.itemservice.validation;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.validation.DefaultMessageCodesResolver;
+import org.springframework.validation.MessageCodesResolver;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class MessageCondesResolverTest {
+
+    MessageCodesResolver codesResolver = new DefaultMessageCodesResolver();
+
+    //단순히 주어진문자열과 객체 (required, item)로 어떻게 messageCodes가 생성되는지 보여주는 코드이다.
+    @Test
+    void messageCodesResolverObject() {
+        String[] messageCodes = codesResolver.resolveMessageCodes("required", "item");
+        for (String messageCode : messageCodes) {
+            System.out.println("messageCode = " + messageCode);
+        }
+        assertThat(messageCodes).containsExactly("required.item", "required");
+    }
+
+    @Test
+    void messageCodesResolverField() {
+        String[] messageCodes = codesResolver.resolveMessageCodes("required", "item", "itemName", String.class);
+        for (String messageCode : messageCodes) {
+            System.out.println("messageCode = " + messageCode);
+        }
+        assertThat(messageCodes).containsExactly(
+                "required.item.itemName",
+                "required.itemName",
+                "required.java.lang.String",
+                "required"
+        );
+    }
+}
