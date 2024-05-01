b5313150241c3b3b89f0bae572b25671a4119e82
YeachaN <92733150+chans27@users.noreply.github.com>
2024-05-02 00:49:03 +0900

    transaction template

--- spring_db/src/main/java/hello/jdbc/service/MemberServiceV3_2.java
+++ spring_db/src/main/java/hello/jdbc/service/MemberServiceV3_2.java
@@ -2,11 +2,8 @@ package hello.jdbc.service;
 
 import hello.jdbc.domain.Member;
 import hello.jdbc.repository.MemberRepositoryV3;
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.transaction.PlatformTransactionManager;
-import org.springframework.transaction.TransactionStatus;
-import org.springframework.transaction.support.DefaultTransactionDefinition;
 import org.springframework.transaction.support.TransactionTemplate;
 
 import java.sql.Connection;
@@ -16,15 +13,14 @@ import java.sql.SQLException;
  * 트랜잭션 - 트랜잭션 템플릿
  */
 @Slf4j
-@RequiredArgsConstructor
 public class MemberServiceV3_2 {
 
 //    private final PlatformTransactionManager transactionManager;
     private final TransactionTemplate txTemplate;
     private final MemberRepositoryV3 memberRepository;
 
-    public MemberServiceV3_2(TransactionTemplate transactionManager, MemberRepositoryV3 memberRepository) {
-        this.txTemplate = new TransactionTemplate();
+    public MemberServiceV3_2(PlatformTransactionManager transactionManager, MemberRepositoryV3 memberRepository) {
+        this.txTemplate = new TransactionTemplate(transactionManager);
         this.memberRepository = memberRepository;
     }
 
