6d2b1f5a398a19b892b525aa3557f425be931cd3
YeachaN <92733150+chans27@users.noreply.github.com>
2024-06-02 21:37:57 +0900

    JPA

--- spring_db2/itemservice-db/src/main/java/hello/itemservice/config/MyBatisConfig.java
+++ spring_db2/itemservice-db/src/main/java/hello/itemservice/config/MyBatisConfig.java
@@ -9,7 +9,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-@Configurationd
+@Configuration
 @RequiredArgsConstructor
 public class MyBatisConfig {
 
