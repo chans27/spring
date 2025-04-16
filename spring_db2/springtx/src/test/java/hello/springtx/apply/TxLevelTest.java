d4a7a30d23da9f571797dcb223ba0d622fa015bd
chans27 <ever1275@naver.com>
2025-04-16 21:53:15 +0900

    transaction level test

--- spring_db2/springtx/src/test/java/hello/springtx/apply/TxLevelTest.java
+++ spring_db2/springtx/src/test/java/hello/springtx/apply/TxLevelTest.java
@@ -6,7 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
-import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.transaction.support.TransactionSynchronizationManager;
 
